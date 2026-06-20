934cbeb190492ed01826c18e7179a57d6f95b32c
Spreelo <contact@vifsy.com>
2026-06-20 02:07:20 +0200

    Update page.jsx

--- app/settings/page.jsx
+++ app/settings/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AppLayout from "../../components/AppLayout";
 import { supabase } from "../../lib/supabaseClient";
 
